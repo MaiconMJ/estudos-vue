830221fb43c937ca1b61395fe2cc07c27440c8ed
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-08-09 22:55:23 -0300

    Adicionados mais dados no componentePessoa

--- 2_projeto_vue/src/components/ComponentePessoa.vue
+++ 2_projeto_vue/src/components/ComponentePessoa.vue
@@ -1,5 +1,14 @@
 <template>
-  <h2>Esta é a descrição da pessoa: {{ nome }}</h2>
+  <div>
+    <h2>Esta é a descrição da pessoa: {{ nome }}</h2>
+    <p>Estou trabalhando no momento</p>
+    <p>Utilizo as seguintes tecnologias:</p>
+    <ul>
+      <li>JavaScript</li>
+      <li>PHP</li>
+      <li>Python</li>
+    </ul>
+  </div>
 </template>
 
 <script>
