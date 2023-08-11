1180752aa127f99ad952807b19a4107f4da70be0
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-08-10 22:02:25 -0300

    Criado o ComponenteForm

---
+++ 2_projeto_vue/src/components/ComponenteForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <form action="">
+    <input type="text">
+    <input type="text">
+    <input type="submit" value="Enviar">
+  </form>
+</template>
+
+<script>
+  export default {
+    name: "ComponenteForm"
+  }
+</script>
