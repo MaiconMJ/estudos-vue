d37f3da6dfe13cd975c383c2ac4d9f6bac2b5912
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-08-15 20:22:28 -0300

    Adicionado o lifeCycle() no mounted e no created como teste

--- 2_projeto_vue/src/components/LifeCycle.vue
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -14,11 +14,20 @@ export default {
     setTimeout(() => {
       this.nome = "Maicon";
     }, 1000);
+
+    this.lifeCycle()
   },
   mounted() {
     setTimeout(() => {
       this.nome = "João";
     }, 2000);
+
+    this.lifeCycle()
   },
+  methods: {
+    lifeCycle() {
+      console.log("Executou")
+    }
+  }
 };
 </script>
