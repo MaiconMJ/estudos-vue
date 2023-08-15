9d13db5d0806876cac739b6e2ef700e3a8d7e42f
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-08-15 20:51:43 -0300

    Adicionado o ComponenteHeader

---
+++ 2_projeto_vue/src/components/ComponenteHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Projetos</a></li>
+    <li><a href="#">Contato</a></li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "ComponenteHeader"
+  }
+</script>
+
